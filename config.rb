1147b620e2bee03ccac028ebcdb94c3e6159d212
Prateek Dayal <prateek@supportbee.com>
2013-12-10 13:23:19 +0530

    reusing the layout

--- config.rb
+++ config.rb
@@ -24,6 +24,8 @@
 #   page "/admin/*"
 # end
 
+page "/chapters/*", :layout => 'chapters'
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
